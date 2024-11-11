5215123665af16208d6b7af828e38eeb07aaab0a
Amiko Arnoup <your-email@example.com>
2024-11-11 20:52:05 +0400

    adding dynamic routes for jobs

---
+++ resources/views/job.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+
+          <h1><strong>{{$job['title']}}</strong></h1>
+            <strong>This job Payes {{$job['salary']}} per year.
+         
+    
+
+    </body>
+</html>
+
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        @foreach ($jobs as $job)
+            <li>
+            <a href="/jobs/">
+            <strong>{{$job['title']}}:</strong> Payes {{$job['salary']}} per year.
+            </a>
+            </li>
+        @endforeach
+    </body>
+</html>
+
